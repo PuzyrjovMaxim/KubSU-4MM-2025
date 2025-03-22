6cc3bb0afa88fec31d19fef5a209d8c5d076493f
Maxim Puzyrjov <maksimfer@gmail.com>
2025-03-22 10:14:59 +0300

    Teg bot for DeepSeek 6.1

--- src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
+++ src/main/java/ru/kubsu/telegrambot/TelegrambotApplication.java
@@ -2,15 +2,23 @@ package ru.kubsu.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.kubsu.telegrambot.controller.TestController;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import ru.kubsu.telegrambot.ai.deepseek.DeepSeekClient;
+import ru.kubsu.telegrambot.bot.MyAmazingBot;
 
 @SpringBootApplication
 public class TelegrambotApplication {
 
-	public static void main(String[] args) {
-		TestController controller = new TestController();
-		String hello = controller.hello("name");
-		System.out.println(hello);
+	private static final String TELEGRAM_BOT_TOKEN = "7983066682:AAGdJrGsDJjQu_gufxi5pOqTwUJCix8EjfI";
+
+	public static void main(String[] args) throws Exception {
+		// Telegram
+		TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+		botsApplication.registerBot(TELEGRAM_BOT_TOKEN,
+				new MyAmazingBot(
+						new OkHttpTelegramClient(TELEGRAM_BOT_TOKEN),
+						new DeepSeekClient()));
 
 		SpringApplication.run(TelegrambotApplication.class, args);
 	}
